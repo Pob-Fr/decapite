67625dbe5fd6516d3bf8273c8759f34d99568dab
Daratrixx <zeratul_du_38@hotmail.fr>
2017-11-26 10:41:28 +0100

    Inputs

---
+++ Assets/Scripts/CreditController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Attack") || Input.GetButton("Menu")) {
+            SceneManager.LoadScene("Scenes/MainMenu");
+        } else if (Input.GetButton("Quit")) {
+            Application.Quit();
+        }
+	}
+}
