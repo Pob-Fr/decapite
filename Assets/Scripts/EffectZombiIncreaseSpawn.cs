f83647802fa6b064c4d34299f993a0d61b2ddafe
Daratrixx <zeratul_du_38@hotmail.fr>
2017-11-27 14:57:11 +0100

    Renamed effect more zombi, event text changed to Endless.

---
+++ Assets/Scripts/EffectZombiIncreaseSpawn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class EffectZombiIncreaseSpawn : Effect {
+    int zombiSpawnIncr = 1;
+
+    public EffectZombiIncreaseSpawn(int incr) {
+        this.zombiSpawnIncr = incr;
+    }
+
+    public void ChangeNbZombiIncr(int incr) {
+        this.zombiSpawnIncr = incr;
+    }
+
+    public bool isBonus() {
+        return false;
+    }
+
+    public bool isMalus() {
+        return true;
+    }
+
+    public void DoSomething() {
+        GameDirector.singleton.IncreaseZombiSpawnCount(zombiSpawnIncr);
+        GameDirector.singleton.Event("<color=#FF0000>Endless !</color>");
+    }
+}
