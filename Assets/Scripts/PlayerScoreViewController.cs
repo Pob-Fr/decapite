e61a60cf1eaaea2b8423061391869cd08a6d2a62
Daratrixx <zeratul_du_38@hotmail.fr>
2017-11-29 14:59:14 +0100

    WIP - unstash git add .!

---
+++ Assets/Scripts/PlayerScoreViewController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScoreViewController : MonoBehaviour {
+
+    public UnityEngine.UI.Text scoreDisplayer;
+    public UnityEngine.UI.Text timerDisplayer;
+    public UnityEngine.UI.Text lifeDisplayer;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
